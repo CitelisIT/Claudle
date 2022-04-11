3e7dbd3f13c9e26f82a4f5d559cc8b2fb908a687
Guillaume <=>
2022-04-11 15:33:41 +0200

    [REFACTOR] Tests are now done using assertions

--- api/test_validate.py
+++ api/test_validate.py
@@ -6,6 +6,7 @@ letters1 = 4
 word11 = "that"
 word12 = "what"
 word13 = "watt"
+word14 = "join"
 
 
 gamemode2 = "francais"
@@ -17,21 +18,25 @@ word23 = "force"
 word24 = "poche"
 
 def test_acceptable():
-    print(acceptable(word11, gamemode1, letters1))
-    print(acceptable(word12, gamemode1, letters1))
-    print(acceptable(word13, gamemode1, letters1))
-    print(acceptable(word21, gamemode2, letters2))
-    print(acceptable(word22, gamemode2, letters2))
-    print(acceptable(word23, gamemode2, letters2))
-    print(acceptable(word24, gamemode2, letters2))
+    assert acceptable(word11, gamemode1, letters1) == True
+    assert acceptable(word12, gamemode1, letters1) == True
+    assert acceptable(word13, gamemode1, letters1) == True
+    assert acceptable(word21, gamemode2, letters2) == True
+    assert acceptable(word22, gamemode2, letters2) == False
+    assert acceptable(word23, gamemode2, letters2) == True
+    assert acceptable(word24, gamemode2, letters2) == True
 
 def test_verification():
-    print(verification(word11, gamemode1, letters1, target1))
-    print(verification(word12, gamemode1, letters1, target1))
-    print(verification(word13, gamemode1, letters1, target1))
-    print(verification(word21, gamemode2, letters2, target2))
-    print(verification(word22, gamemode2, letters2, target2))
-    print(verification(word23, gamemode2, letters2, target2))
-    print(verification(word24, gamemode2, letters2, target2))
+    assert verification(word11, gamemode1, letters1, target1) == [1,0,1,2]
+    assert verification(word12, gamemode1, letters1, target1) == [2,0,1,2]
+    assert verification(word13, gamemode1, letters1, target1)  == [2,2,2,2]
+    assert verification(word14,gamemode1, letters1, target1) == [0,0,0,0]
+    assert verification(word21, gamemode2, letters2, target2) == [2, 0, 0, 0, 2]
+    assert verification(word22, gamemode2, letters2, target2) == [0, 0, 0, 0, 0]
+    assert verification(word23, gamemode2, letters2, target2) == [0, 2, 0, 1, 2]
+    assert verification(word24, gamemode2, letters2, target2) == [2, 2, 2, 2, 2]
 
+
+test_acceptable()
 test_verification()
+print("All tests passed with success \n")
