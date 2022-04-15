163e176bcecb1b5bb7da2f05d4ac728f7c896b58
BENE Tom <tom.bene@telecomnancy.eu>
2022-04-15 19:18:01 +0200

    [add] added component for letter grid square

---
+++ frontend/src/components/LetterSquare.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+interface Props {
+  letter: string;
+  hint: number;
+}
+
+export default function LetterSquare({ letter, hint }: Props) {
+  var className = "";
+  switch (hint) {
+    case 1: {
+      className = "letter-square--misplaced";
+      break;
+    }
+    case 2: {
+      className = "letter-square--correct";
+      break;
+    }
+    default: {
+      className = "letter-square--default";
+    }
+  }
+  return (
+    <div className={className}>
+      <span className="letter-square__letter">{letter.toUpperCase()}</span>
+    </div>
+  );
+}
