8ef1dd8f4025817a3d9265b06718fb3f43135f1e
BENE Tom <tom.bene@telecomnancy.eu>
2022-04-15 23:22:44 +0200

    [add] added routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage";
+import RegisterPage from "./pages/RegisterPage";
+import StatsPage from "./pages/StatsPage";
 
 export default function App() {
-  return <div className="app-wrapper"></div>;
+  return (
+    <Router>
+      <div className="app-wrapper">
+        <Routes>
+          <Route path="" element={<HomePage />} />
+          <Route path="profile" element={<ProfilePage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/stats" element={<StatsPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
