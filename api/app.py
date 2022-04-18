eada108524c6f567bc2dc1280cb7f023565567b2
Camille Noiray-Moussu <camille.noiray-moussu@telecomnancy.eu>
2022-04-18 13:00:01 +0200

    [add] added function using validateRefactor and wordSelect for each corresponding route

--- api/app.py
+++ api/app.py
@@ -1,4 +1,5 @@
-from validate import *
+from validateRefactor import *
+from wordSelect import *
 from flask import Flask
 from flask.helpers import flash
 from flask_sqlalchemy import SQLAlchemy
@@ -7,6 +8,7 @@ from hashlib import sha256
 from datetime import datetime
 from sqlalchemy import or_, desc
 import pickle
+
 
 from sqlalchemy.orm import query
 from sqlalchemy.sql.functions import current_timestamp
@@ -19,6 +21,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+from models import * 
 
 db.create_all()
 
@@ -27,18 +30,39 @@ db.create_all()
 def validate():
     args = request.args
     word = args['word']
-    mode = args['mode']
+    language = args['language']
     target = args['target']
-    if not acceptable(word, mode, len(target)):
-        if mode == "francais":
+    words = getWords(len(target), language)
+    result = [0 in range (len(target))]
+    if not validWord(word,words):
+        if language == "francais":
             return "Ce mot n'est pas dans notre dictionnaire"
-        if mode == "anglais":
-            return "Ce mot n'est pas dans notre dictionnaire"
+        if language == "english":
+            return "this word isn't in our dictionnary"
     else:
-        return verification(word, mode, len(target), target)
+        result = verification(word, target)
+    response_body = {
+        "hint": result
+    }
+    return response_body
 
 
 @app.route('/api/getword', methods=['GET'])
+def getword():
+    args = request.args
+    language = args['language']
+    length = args['length']
+    if session.get('user_id') is None:
+        wordsMet = []
+        result = select(length, language, wordsMet)
+        response_body = {"words": result }
+        return response_body
+    user = User.query.filter(User.user_id==session.get('user_id')).first()
+    wordsMet = Games.query.join(user, Games.user_Id == User.Id).add_columns(Games.word).filter(User.Id==Games.user_Id).all()
+    result = select(length, language, wordsMet)
+    response_body = {"words": result }
+    return response_body
+
 @app.route('/api/register', methods=['POST'])
 @app.route('/api/login', methods=['POST'])
 @app.route('/api/profile', methods=['GET'])
@@ -49,3 +73,5 @@ def profile():
 
 
 @app.route('/api/stats', methods=['GET'])
+def getStats():
+    args = request.args
