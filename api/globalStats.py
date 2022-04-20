d6d25f264feb447546706d27d745ac570506519f
Guillaume RICARD <guillaume.ricard@telecomnancy.eu>
2022-04-20 18:08:43 +0200

    [refactor] globalstats.py now uses SQLAchemy

--- api/globalStats.py
+++ api/globalStats.py
@@ -1,39 +1,40 @@
-##Considering we are already connected to the DB and c is a cursor
-
+from models import *
 
 #Global Stats
 def numberOfGames():
-    c.execute("SELECT COUNT(*) FROM Games")
-    return c.fetchone()[0]
+    number = Games.query.count()
+    return number
 
 def percentageOfWonGames():
-    c.execute("SELECT COUNT(*) FROM Games WHERE Tries_Num != -1")
-    return c.fetchone()[0]/numberOfGames()*100
+    number = Games.query.filter(Games.Tries_Num != -1).count()
+    return number/numberOfGames()*100
 
 def numberOfWonGamesTriesNumber(TriesNumber):
-    c.execute("SELECT COUNT(*) FROM Games WHERE Tries_Num = %s" % TriesNumber)
-    return c.fetchone()[0]
+    number = Games.query.filter(Games.Tries_Num == TriesNumber).count()
+    return number
+
 
 
 #User Stats
 
 def numberOfGamesByUser(UserId):
-    c.execute("SELECT COUNT(*) FROM Games WHERE User_Id = %s" % UserId)
-    return c.fetchone()[0]
+    number = Games.query.filter(Games.User_Id == UserId).count()
+    return number
+
 
 def percentageOfWonGamesByUser(UserId):
-    c.execute("SELECT COUNT(*) FROM Games WHERE Tries_Num != -1 AND User_Id = %s" % UserId)
-    return c.fetchone()[0]/numberOfGamesByUser(UserId)*100
+    number = Games.query.filter(Games.User_Id == UserId).filter(Games.Tries_Num != -1).count()
+    return number/numberOfGamesByUser(UserId)*100
 
 def numberOfWonGamesTriesNumberByUser(UserId, TriesNumber):
-    c.execute("SELECT COUNT(*) FROM Games WHERE Tries_Num = %s AND User_Id = %s" % (TriesNumber, UserId))
-    return c.fetchone()[0]
+    number = Games.query.filter(Games.User_Id == UserId).filter(Games.Tries_Num == TriesNumber).count()
+    return number
 
 def currentStreakByUser(UserId):
     streak = 0
-    c.execute("SELECT Tries_Num FROM Games WHERE User_Id = %s ORDER BY Game_Id DESC" % UserId)
-    for i in range(len(c.fetchall())):
-        if c.fetchone()[0] != -1:
+    result = Games.query.filter(Games.Tries_Num).filter(Games.User_Id == UserId).order_by(Games.Game_Id.desc()).all()
+    for i in range(result):
+        if result[i] != -1:
             streak += 1
         else:
             break
@@ -42,13 +43,12 @@ def currentStreakByUser(UserId):
 def bestStreakByUser(UserId):
     BestStreak = 0
     Streak = 0
-    c.execute("SELECT Tries_Num FROM Games WHERE User_Id = %s ORDER BY Game_Id DESC" % UserId)
-    for i in range(len(c.fetchall())):
-        if c.fetchall()[i] != -1:
+    result = Games.query.filter(Games.Tries_Num).filter(Games.User_Id == UserId).order_by(Games.Game_Id.desc()).all()
+    for i in range(len(result)):
+        if result[i] != -1:
             Streak += 1
         else:
             BestStreak = Streak if Streak > BestStreak else BestStreak
             Streak = 0
     return BestStreak if BestStreak > Streak else Streak
-
 
