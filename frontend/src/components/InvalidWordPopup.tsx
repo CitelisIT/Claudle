621fd0b5cfac33a7f2078b711a2dfb407757f806
Alexandre LI <alexandre.li@telecomnancy.eu>
2022-04-25 21:41:37 +0200

    [Refactor] Invalid word popup is now centered
    TODO: Make it transition and tweak colors

--- frontend/src/components/InvalidWordPopup.tsx
+++ frontend/src/components/InvalidWordPopup.tsx
@@ -15,9 +15,9 @@ export default function InvalidWordPopup({
     open = {badWord}
     onClose={() => setBadWord(false)}
     >
-          <Dialog.Title  className="fixed flex flex-col items-center justify-center gap-4 bg-gray-100 border border-gray-600 rounded-lg shadow-2xl top-20 left-4 right-4 md:w-3/12">
-            <div className="flex justify-between w-full itemns-center">
-              <h2 className="text-base font-semibold text-slate-700 md:text-lg lg:text-xl">
+          <Dialog.Title  className="fixed m-auto items-center justify-center gap-4 bg-gray-100 border border-gray-600 rounded-lg top-20 left-4 right-4 md:w-3/12">
+            <div className="flex justify-center w-auto items-center">
+              <h2 className="text-base items-center font-semibold text-slate-700 md:text-lg lg:text-xl">
                 Invalid word
               </h2>
             </div>
