3ada848718283025c89489489c90283ecb0b67a5
Alexandre LI <alexandre.li@telecomnancy.eu>
2022-04-26 14:35:30 +0200

    [Refactor] Tweaked background color of popup

--- frontend/src/components/InvalidWordPopup.tsx
+++ frontend/src/components/InvalidWordPopup.tsx
@@ -1,5 +1,4 @@
 import { Dialog, Popover, Transition } from "@headlessui/react";
-import { QuestionMarkCircleIcon, XIcon } from "@heroicons/react/outline";
 
 
 interface Props {
@@ -8,20 +7,20 @@ interface Props {
 }
 
 export default function InvalidWordPopup({
-    badWord,setBadWord
+    badWord, setBadWord
 }: Props) {
   return (
     <Dialog
     open = {badWord}
     onClose={() => setBadWord(false)}
     >
-          <Dialog.Title  className="fixed m-auto items-center justify-center gap-4 bg-gray-100 border border-gray-600 rounded-lg top-20 left-4 right-4 md:w-3/12">
-            <div className="flex justify-center w-auto items-center">
-              <h2 className="text-base items-center font-semibold text-slate-700 md:text-lg lg:text-xl">
-                Invalid word
-              </h2>
-            </div>
-        </Dialog.Title>
+      <Dialog.Title  className="fixed m-auto items-center justify-center gap-4 bg-slate-200 border border-gray-800 rounded-lg top-20 left-4 right-4 md:w-3/12">
+        <div className="flex justify-center w-auto items-center">
+          <h2 className="text-base items-center font-semibold text-slate-700 md:text-lg lg:text-xl">
+            Invalid word
+          </h2>
+        </div>
+      </Dialog.Title>
     </Dialog>
   );
 }
