91d01e315652b26d1ef777a13eb404762a0cc638
BENE Tom <tom.bene@telecomnancy.eu>
2022-04-27 08:19:48 +0200

    [refactor] changed popup color

--- frontend/src/components/InvalidWordPopup.tsx
+++ frontend/src/components/InvalidWordPopup.tsx
@@ -1,25 +1,17 @@
-import { Dialog, Popover, Transition } from "@headlessui/react";
-
+import { Dialog } from "@headlessui/react";
 
 interface Props {
-    badWord: boolean;
-    setBadWord:  React.Dispatch<React.SetStateAction<boolean>>;
+  badWord: boolean;
+  setBadWord: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export default function InvalidWordPopup({
-    badWord, setBadWord
-}: Props) {
+export default function InvalidWordPopup({ badWord, setBadWord }: Props) {
   return (
-    <Dialog
-    open = {badWord}
-    onClose={() => setBadWord(false)}
-    >
-      <Dialog.Title  className="fixed m-auto items-center justify-center gap-4 bg-slate-200 border border-gray-800 rounded-lg top-20 left-4 right-4 md:w-3/12">
-        <div className="flex justify-center w-auto items-center">
-          <h2 className="text-base items-center font-semibold text-slate-700 md:text-lg lg:text-xl">
-            Invalid word
-          </h2>
-        </div>
+    <Dialog open={badWord} onClose={() => setBadWord(false)}>
+      <Dialog.Title className="fixed flex items-center justify-center p-1 m-auto bg-gray-700 border border-gray-800 rounded-lg top-20 left-4 right-4 md:w-3/12">
+        <h2 className="items-center text-base font-semibold text-gray-200 md:text-lg lg:text-xl">
+          Mot invalide
+        </h2>
       </Dialog.Title>
     </Dialog>
   );
