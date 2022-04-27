d314f4ce1c819e3544eb1391553c8665517619b0
BENE Tom <tom.bene@telecomnancy.eu>
2022-04-27 17:27:15 +0200

    [fix] fix number alignement on graph

--- frontend/src/components/Graph.tsx
+++ frontend/src/components/Graph.tsx
@@ -4,7 +4,7 @@ import axios from "axios";
 export default function Graph() {
   const URL = "http://127.0.0.1:5000/api/stats";
   const [stats, setStats] = useState({
-    winsByTries: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    winsByTries: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   });
 
   useEffect(() => {
@@ -31,91 +31,91 @@ export default function Graph() {
     <div className="flex flex-col items-center justify-center w-full">
       <div className="flex flex-col items-start justify-center w-full gap-4 my-8 a -lg lg:text-xl">
         <div className="flex flex-row items-center justify-start w-full gap-2">
-          <span>1</span>
+          <span className="w-8 text-right">1</span>
           <div
             style={{ width: normOne }}
-            className={`py-1 min-w-fit text-right bg-green-600 rounded-sm text-end px-2`}
+            className={`py-1 min-w-fit text-right bg-green-600 rounded-sm px-2`}
           >
             {stats.winsByTries[0]}
           </div>
         </div>
-        <div className="flex flex-row items-center justify-start w-full gap-2">
-          <span>2</span>
+        <div className="flex flex-row items-center justify-start w-full gap-2 text-right">
+          <span className="w-8 text-right">2</span>
           <div
             style={{ width: normTwo }}
-            className={`py-1 min-w-fit text-right bg-green-600 rounded-sm text-end px-2`}
+            className={`py-1 min-w-fit bg-green-600 rounded-sm px-2`}
           >
             {stats.winsByTries[1]}
           </div>
         </div>
-        <div className="flex flex-row items-center justify-start w-full gap-2">
-          <span>3</span>
+        <div className="flex flex-row items-center justify-start w-full gap-2 text-right">
+          <span className="w-8 text-right">3</span>
           <div
             style={{ width: normThree }}
-            className={`py-1 min-w-fit text-right bg-green-600 rounded-sm text-end px-2`}
+            className={`py-1 min-w-fit bg-green-600 rounded-sm px-2`}
           >
             {stats.winsByTries[2]}
           </div>
         </div>
-        <div className="flex flex-row items-center justify-start w-full gap-2">
-          <span>4</span>
+        <div className="flex flex-row items-center justify-start w-full gap-2 text-right">
+          <span className="w-8 text-right">4</span>
           <div
             style={{ width: normFour }}
-            className={`py-1 min-w-fit text-right bg-green-600 rounded-sm text-end px-2`}
+            className={`py-1 min-w-fit bg-green-600 rounded-sm px-2`}
           >
             {stats.winsByTries[3]}
           </div>
         </div>
-        <div className="flex flex-row items-center justify-start w-full gap-2">
-          <span>5</span>
+        <div className="flex flex-row items-center justify-start w-full gap-2 text-right">
+          <span className="w-8 text-right">5</span>
           <div
             style={{ width: normFive }}
-            className={`py-1 min-w-fit text-right bg-green-600 rounded-sm text-end px-2`}
+            className={`py-1 min-w-fit bg-green-600 rounded-sm px-2`}
           >
             {stats.winsByTries[4]}
           </div>
         </div>
-        <div className="flex flex-row items-center justify-start w-full gap-2">
-          <span>6</span>
+        <div className="flex flex-row items-center justify-start w-full gap-2 text-right">
+          <span className="w-8 text-right">6</span>
           <div
             style={{ width: normSix }}
-            className={`py-1 min-w-fit text-right bg-green-600 rounded-sm text-end px-2`}
+            className={`py-1 min-w-fit bg-green-600 rounded-sm px-2`}
           >
             {stats.winsByTries[5]}
           </div>
         </div>
-        <div className="flex flex-row items-center justify-start w-full gap-2">
-          <span>7</span>
+        <div className="flex flex-row items-center justify-start w-full gap-2 text-right">
+          <span className="w-8 text-right">7</span>
           <div
             style={{ width: normSeven }}
-            className={`py-1 min-w-fit text-right bg-green-600 rounded-sm text-end px-2`}
+            className={`py-1 min-w-fit bg-green-600 rounded-sm px-2`}
           >
             {stats.winsByTries[6]}
           </div>
         </div>
-        <div className="flex flex-row items-center justify-start w-full gap-2">
-          <span>8</span>
+        <div className="flex flex-row items-center justify-start w-full gap-2 text-right">
+          <span className="w-8 text-right">8</span>
           <div
             style={{ width: normEight }}
-            className={`py-1 min-w-fit text-right bg-green-600 rounded-sm text-end px-2`}
+            className={`py-1 min-w-fit text-right bg-green-600 rounded-sm px-2`}
           >
             {stats.winsByTries[7]}
           </div>
         </div>
-        <div className="flex flex-row items-center justify-start w-full gap-2">
-          <span>9</span>
+        <div className="flex flex-row items-center justify-start w-full gap-2 text-right">
+          <span className="w-8 text-right">9</span>
           <div
             style={{ width: normNine }}
-            className={`py-1 min-w-fit text-right bg-green-600 rounded-sm text-end px-2`}
+            className={`py-1 min-w-fit bg-green-600 rounded-sm px-2`}
           >
             {stats.winsByTries[8]}
           </div>
         </div>
-        <div className="flex flex-row items-center justify-start w-full gap-2">
-          <span>10</span>
+        <div className="flex flex-row items-center justify-start w-full gap-2 text-right">
+          <span className="w-8 text-right">10</span>
           <div
             style={{ width: normTen }}
-            className={`py-1 min-w-fit text-right bg-green-600 rounded-sm text-end px-2`}
+            className={`py-1 min-w-fit bg-green-600 rounded-sm px-2`}
           >
             {stats.winsByTries[9]}
           </div>
