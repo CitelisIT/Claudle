8bc4099381820c3ab61b42f52dfc5ef0b6a2e61e
BENE Tom <tom.bene@telecomnancy.eu>
2022-04-27 20:25:53 +0200

    [refactor] passed props to graph instead of making api call

--- frontend/src/components/Graph.tsx
+++ frontend/src/components/Graph.tsx
@@ -1,32 +1,24 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
-export default function Graph() {
-  const URL = "http://127.0.0.1:5000/api/stats";
-  const [stats, setStats] = useState({
-    winsByTries: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  });
+interface Props {
+  winsByTries: number[];
+}
 
-  useEffect(() => {
-    axios.get(URL).then((response) => {
-      setStats(response.data);
-    });
-  }, []);
-  if (!stats) return null;
-  const max = stats.winsByTries.reduce(
-    (op, item) => (op = op > item ? op : item),
+export default function Graph({ winsByTries }: Props) {
+  const max = winsByTries.reduce(
+    (op: any, item: any) => (op = op > item ? op : item),
     0
   );
-  const normOne = Math.floor((stats.winsByTries[0] / max) * 100) + "%";
-  const normTwo = Math.floor((stats.winsByTries[1] / max) * 100) + "%";
-  const normThree = Math.floor((stats.winsByTries[2] / max) * 100) + "%";
-  const normFour = Math.floor((stats.winsByTries[3] / max) * 100) + "%";
-  const normFive = Math.floor((stats.winsByTries[4] / max) * 100) + "%";
-  const normSix = Math.floor((stats.winsByTries[5] / max) * 100) + "%";
-  const normSeven = Math.floor((stats.winsByTries[6] / max) * 100) + "%";
-  const normEight = Math.floor((stats.winsByTries[7] / max) * 100) + "%";
-  const normNine = Math.floor((stats.winsByTries[8] / max) * 100) + "%";
-  const normTen = Math.floor((stats.winsByTries[9] / max) * 100) + "%";
+  const normOne = Math.floor((winsByTries[0] / max) * 100) + "%";
+  const normTwo = Math.floor((winsByTries[1] / max) * 100) + "%";
+  const normThree = Math.floor((winsByTries[2] / max) * 100) + "%";
+  const normFour = Math.floor((winsByTries[3] / max) * 100) + "%";
+  const normFive = Math.floor((winsByTries[4] / max) * 100) + "%";
+  const normSix = Math.floor((winsByTries[5] / max) * 100) + "%";
+  const normSeven = Math.floor((winsByTries[6] / max) * 100) + "%";
+  const normEight = Math.floor((winsByTries[7] / max) * 100) + "%";
+  const normNine = Math.floor((winsByTries[8] / max) * 100) + "%";
+  const normTen = Math.floor((winsByTries[9] / max) * 100) + "%";
   return (
     <div className="flex flex-col items-center justify-center w-full">
       <div className="flex flex-col items-start justify-center w-full gap-4 my-8 a -lg lg:text-xl">
@@ -36,7 +28,7 @@ export default function Graph() {
             style={{ width: normOne }}
             className={`py-1 min-w-fit text-right bg-green-600 rounded-sm px-2`}
           >
-            {stats.winsByTries[0]}
+            {winsByTries[0]}
           </div>
         </div>
         <div className="flex flex-row items-center justify-start w-full gap-2 text-right">
@@ -45,7 +37,7 @@ export default function Graph() {
             style={{ width: normTwo }}
             className={`py-1 min-w-fit bg-green-600 rounded-sm px-2`}
           >
-            {stats.winsByTries[1]}
+            {winsByTries[1]}
           </div>
         </div>
         <div className="flex flex-row items-center justify-start w-full gap-2 text-right">
@@ -54,7 +46,7 @@ export default function Graph() {
             style={{ width: normThree }}
             className={`py-1 min-w-fit bg-green-600 rounded-sm px-2`}
           >
-            {stats.winsByTries[2]}
+            {winsByTries[2]}
           </div>
         </div>
         <div className="flex flex-row items-center justify-start w-full gap-2 text-right">
@@ -63,7 +55,7 @@ export default function Graph() {
             style={{ width: normFour }}
             className={`py-1 min-w-fit bg-green-600 rounded-sm px-2`}
           >
-            {stats.winsByTries[3]}
+            {winsByTries[3]}
           </div>
         </div>
         <div className="flex flex-row items-center justify-start w-full gap-2 text-right">
@@ -72,7 +64,7 @@ export default function Graph() {
             style={{ width: normFive }}
             className={`py-1 min-w-fit bg-green-600 rounded-sm px-2`}
           >
-            {stats.winsByTries[4]}
+            {winsByTries[4]}
           </div>
         </div>
         <div className="flex flex-row items-center justify-start w-full gap-2 text-right">
@@ -81,7 +73,7 @@ export default function Graph() {
             style={{ width: normSix }}
             className={`py-1 min-w-fit bg-green-600 rounded-sm px-2`}
           >
-            {stats.winsByTries[5]}
+            {winsByTries[5]}
           </div>
         </div>
         <div className="flex flex-row items-center justify-start w-full gap-2 text-right">
@@ -90,7 +82,7 @@ export default function Graph() {
             style={{ width: normSeven }}
             className={`py-1 min-w-fit bg-green-600 rounded-sm px-2`}
           >
-            {stats.winsByTries[6]}
+            {winsByTries[6]}
           </div>
         </div>
         <div className="flex flex-row items-center justify-start w-full gap-2 text-right">
@@ -99,7 +91,7 @@ export default function Graph() {
             style={{ width: normEight }}
             className={`py-1 min-w-fit text-right bg-green-600 rounded-sm px-2`}
           >
-            {stats.winsByTries[7]}
+            {winsByTries[7]}
           </div>
         </div>
         <div className="flex flex-row items-center justify-start w-full gap-2 text-right">
@@ -108,7 +100,7 @@ export default function Graph() {
             style={{ width: normNine }}
             className={`py-1 min-w-fit bg-green-600 rounded-sm px-2`}
           >
-            {stats.winsByTries[8]}
+            {winsByTries[8]}
           </div>
         </div>
         <div className="flex flex-row items-center justify-start w-full gap-2 text-right">
@@ -117,7 +109,7 @@ export default function Graph() {
             style={{ width: normTen }}
             className={`py-1 min-w-fit bg-green-600 rounded-sm px-2`}
           >
-            {stats.winsByTries[9]}
+            {winsByTries[9]}
           </div>
         </div>
       </div>
