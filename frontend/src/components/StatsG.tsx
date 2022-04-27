4aff6e66913e6faf00cc34ab994d09de16d69eb2
Camille Noiray-Moussu <camille.noiray-moussu@telecomnancy.eu>
2022-04-27 21:18:24 +0200

    [fix] fix data import for stat page following Tom s logic

--- frontend/src/components/StatsG.tsx
+++ frontend/src/components/StatsG.tsx
@@ -1,43 +1,38 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { number } from "yup";
 
-export default function StatsG() {
-  const [stats, setStats] = useState({
-    nbGamesPlayed: 0,
-    percGamesWon: 0,
-    bestStreak: 0,
-    bestUser: "",
-    winsByTries: [0, 0, 0, 0, 2, 0, 0, 0, 0],
-  });
+interface Props {
+  nbGamesPlayed: number;
+  percGamesWon: number;
+  bestUser: string;
+  bestStreak: number;
+}
 
-  useEffect(() => {
-    axios.get("/api/stats").then((response) => {
-      setStats(response.data);
-    });
-  }, []);
-  if (!stats) return null;
+export default function StatsG({
+  nbGamesPlayed,
+  percGamesWon,
+  bestUser,
+  bestStreak,
+}: Props) {
   return (
     <section id="stats" className="flex items-center justify-center">
       <div className="flex flex-col items-center justify-center gap-4 text-gray-200 text-md md:text-lg lg:text-xl md:items-start">
         <div className="flex flex-col items-center justify-center md:flex-row">
           <div className="flex flex-col items-center justify-center gap-2 p-4">
-            <span className="text-3xl font-semibold">
-              {stats.nbGamesPlayed}
-            </span>
+            <span className="text-3xl font-semibold">{nbGamesPlayed}</span>
             <span className="font-light">Parties Jouées</span>
           </div>
           <div className="flex flex-col items-center justify-center gap-2 p-4">
-            <span className="text-3xl font-semibold">
-              {stats.percGamesWon}%
-            </span>
+            <span className="text-3xl font-semibold">{percGamesWon}%</span>
             <span className="font-light"> Pourcentage De Parties Gagnées</span>
           </div>
           <div className="flex flex-col items-center justify-center gap-2 p-4">
-            <span className="text-3xl font-semibold">{stats.bestUser}</span>
+            <span className="text-3xl font-semibold">{bestUser}</span>
             <span className="font-light">Meilleur joueur</span>
           </div>
           <div className="flex flex-col items-center justify-center gap-2 p-4">
-            <span className="text-3xl font-semibold">{stats.bestStreak}</span>
+            <span className="text-3xl font-semibold">{bestStreak}</span>
             <span className="font-light">Meilleure Série</span>
           </div>
         </div>
--- frontend/src/pages/StatsPage.tsx
+++ frontend/src/pages/StatsPage.tsx
@@ -1,10 +1,26 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import Footer from "../components/Footer";
 import Graph from "../components/Graph";
 import Navbar from "../components/Navbar";
 import StatsG from "../components/StatsG";
+import { stat } from "fs";
 
 export default function StatsPage() {
+  const [stats, setStats] = useState({
+    nbGamesPlayed: 0,
+    percGamesWon: 0,
+    bestStreak: 0,
+    bestUser: "",
+    winsByTries: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  });
+
+  useEffect(() => {
+    axios.get("/api/stats").then((response) => {
+      setStats(response.data);
+    });
+  }, []);
+  if (!stats) return null;
   return (
     <>
       <Navbar setPopupOpen={() => {}} />
@@ -15,8 +31,13 @@ export default function StatsPage() {
         <h2 className="mt-4 text-lg md:mt-16 md:mb-6 md:text-xl lg:text-2xl">
           Statistiques :
         </h2>
-        <StatsG />
-        <Graph winsByTries={[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]} />
+        <StatsG
+          nbGamesPlayed={stats.nbGamesPlayed}
+          percGamesWon={stats.percGamesWon}
+          bestUser={stats.bestUser}
+          bestStreak={stats.bestStreak}
+        />
+        <Graph winsByTries={stats.winsByTries} />
       </div>
       <Footer />
     </>
