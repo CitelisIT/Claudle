2a597d8cab72ea565acb64a5ca4d24d6d8991719
BENE Tom <tom.bene@telecomnancy.eu>
2022-04-27 08:33:33 +0200

    [refactor] keyboard starts in azerty for more consistency

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -26,7 +26,7 @@ export const SettingsContext = createContext<SettingsContextInterface>(
 
 export default function App() {
   const [lang, setLang] = useState("francais");
-  const [layout, setLayout] = useState<string>("qwerty");
+  const [layout, setLayout] = useState<string>("azerty");
   const [size, setSize] = useState<number>(5);
   const [tries, setTries] = useState<number>(6);
   const [hardMode, setHardMode] = useState<boolean>(false);
