63d1315ada7529c1a14f05c984cb2ccbbbb77124
Camille Noiray-Moussu <camille.noiray-moussu@telecomnancy.eu>
2022-04-28 17:19:08 +0200

    [add] display history of all games if connected

--- api/history.py
+++ api/history.py
@@ -1,16 +1,16 @@
 from api.models import * 
+from api.validateRefactor import *
 
-separator = '-' #Can be changed depending on what we decide to use when saving in the database
+separator = '/' #Can be changed depending on what we decide to use when saving in the database
 
-def getHistory(UserId, count):
+def getHistory(userId):
     """Given an user id and a count, returns a list consisting of count lists of words used by the user in its last count games"""
-    Query = Games.query.filter(Games.User_Id == UserId).order_by(Games.Game_Id.desc()).limit(count).all()
-    GamesHistory = []
-    History = []
-    for i in range(len(Query)):
-        GamesHistory.append(Query[i].Tries)
-    for j in range(len(GamesHistory)):
-        History.append(GamesHistory[j].split(separator))
-    return History
+    query = Games.query.filter(Games.User_Id == userId).order_by(Games.Game_Id.desc()).all()
+    
+    history = [{"listWord":[list(l) for l in game.Tries.split(separator)], "target": game.Word, "hints":[], "status":("Victoire" if game.Tries_Num > 0 else "Défaite")} for game in query ]
+    for el in history:
+        el["hints"] = [verification("".join(word), el["target"]) for word in el["listWord"]]
+    
+    return history
 
 
