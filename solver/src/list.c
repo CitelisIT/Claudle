a989d8797b7ddf1f61154ea903dd098858958bba
Camille Noiray-Moussu <camille.noiray-moussu@telecomnancy.eu>
2022-05-13 17:43:53 +0200

    [add/fix] modified base list and added struct to make words easier to parse

--- solver/src/list.c
+++ solver/src/list.c
@@ -7,9 +7,20 @@
 #include <stdbool.h>
 #include <assert.h>
 
-list_t *list_create()
+word_t *word_create()
 {
 
+    word_t *new_word = calloc(1, sizeof(word_t));
+
+    if (new_word == NULL)
+    {
+        err(ENOMEM, "can't allocate a new word");
+    }
+
+    return new_word;
+}
+
+list_t* list_create(){
     list_t *new_list = calloc(1, sizeof(list_t));
 
     if (new_list == NULL)
@@ -20,12 +31,12 @@ list_t *list_create()
     return new_list;
 }
 
-void list_destroy(list_t *one_list)
+void word_destroy(word_t *one_word)
 {
 
-    assert(one_list != NULL);
+    assert(one_word != NULL);
 
-    element_t *current = one_list->head;
+    element_t *current = one_word->head;
     element_t *tmp;
     while (current != NULL)
     {
@@ -33,33 +44,72 @@ void list_destroy(list_t *one_list)
         current = current->next;
         free(tmp);
     }
+    free(one_word);
+}
+
+void list_destroy(list_t *one_list)
+{
+
+    assert(one_list != NULL);
+
+    word_t *current = one_list->sublist;
+    word_t *tmp;
+    while (current != NULL)
+    {
+        tmp = current;
+        current = current->next;
+        word_destroy(tmp);
+    }
     free(one_list);
+}
+
+bool word_is_empty(word_t *one_word)
+{
+    assert(one_word != NULL);
+    return one_word->head == NULL;
 }
 
 bool list_is_empty(list_t *one_list)
 {
     assert(one_list != NULL);
-    return one_list->head == NULL;
+    return one_list->sublist == NULL;
 }
 
-void list_append(list_t *one_list, char one_key, int one_value)
+void word_append(word_t *one_word, char one_key, int one_value)
 {
     element_t *new_element = calloc(1, sizeof(element_t));
     new_element->key = one_key;
     new_element->value = one_value;
     new_element->next = NULL;
-    if (one_list->head == NULL)
+    if (one_word->head == NULL)
     {
-        one_list->head = new_element;
+        one_word->head = new_element;
     }
     else
     {
-        element_t *tmp = one_list->head;
+        element_t *tmp = one_word->head;
         while (tmp->next != NULL)
         {
             tmp = tmp->next;
         }
         tmp->next = new_element;
+    }
+}
+
+void list_append(list_t *one_list, word_t* one_word)
+{
+    if (one_list->sublist == NULL)
+    {
+        one_list->sublist = one_word;
+    }
+    else
+    {
+        word_t *tmp = one_list->sublist;
+        while (tmp->next != NULL)
+        {
+            tmp = tmp->next;
+        }
+        tmp->next = one_word;
     }
 }
 
@@ -69,14 +119,15 @@ void element_print(element_t *one_element)
     printf("%c: %d\n", one_element->key, one_element->value);
 }
 
-void list_print(list_t *one_list)
+
+void word_print(word_t *one_word)
 {
-    assert(one_list != NULL);
+    assert(one_word != NULL);
 
     bool first = true;
 
     printf("[");
-    element_t *current = one_list->head;
+    element_t *current = one_word->head;
     while (current != NULL)
     {
         if (!first)
@@ -95,12 +146,38 @@ void list_print(list_t *one_list)
     printf("]\n");
 }
 
-bool list_contains(list_t *one_list, char one_key)
+void list_print(list_t *one_list)
 {
     assert(one_list != NULL);
+
+    bool first = true;
+
+    printf("[");
+    word_t *current = one_list->sublist;
+    while (current != NULL)
+    {
+        if (!first)
+        {
+            printf(",");
+        }
+        word_print(current);
+        first = false;
+        current = current->next;
+    }
+
+    if (!first)
+    {
+        printf(" ");
+    }
+    printf("]\n");
+}
+
+bool word_contains(word_t *one_word, char one_key)
+{
+    assert(one_word != NULL);
     assert(one_key != NULL);
 
-    element_t *current = one_list->head;
+    element_t *current = one_word->head;
     while (current != NULL)
     {
         if (current->key == one_key)
@@ -113,46 +190,37 @@ bool list_contains(list_t *one_list, char one_key)
     return false;
 }
 
-int list_find(list_t *one_list, char one_key)
+bool list_contains(list_t *one_list, word_t* one_word)
 {
-    element_t *tmp = one_list->head;
-    while (tmp != NULL)
-    {
-        if (tmp->key == one_key)
-        {
-            return (tmp->value);
-        }
-        tmp = tmp->next;
-    }
-    return NULL;
-}
-
-void list_remove_first(list_t *one_list)
-{
+    element_t *letter;
     assert(one_list != NULL);
+    assert(one_word != NULL);
+    bool contain = false;
+    word_t *current = one_list->sublist;
+    while (current != NULL)
+    {   
+        for (size_t i = 0; i < word_get_size(current); i++)
+        {
+            letter = current->head;
+            if (word_contains(current,letter->key))
+            {
+                contain = true;
+            }
+            else{
+                contain = false;
+            }
 
-    element_t *tmp = one_list->head;
-    one_list->head = one_list->head->next;
-    free(tmp);
+        }
+        current = current->next;
+    }
+
+    return contain;
 }
 
-char list_get_key(element_t *one_element)
-{
-    assert(one_element != NULL);
-    return one_element->key;
-}
-
-int list_get_value(element_t *one_element)
-{
-    assert(one_element != NULL);
-    return one_element->value;
-}
-
-int list_get_size(list_t *one_list)
-{
+int list_get_size(list_t *one_list){
     assert(one_list != NULL);
     int size = 0;
-    element_t *tmp = one_list->head;
+    word_t *tmp = one_list->sublist;
     while (tmp != NULL)
     {
         size++;
@@ -161,15 +229,104 @@ int list_get_size(list_t *one_list)
     return size;
 }
 
+
+int word_find(word_t *one_word, char one_key)
+{
+    element_t *tmp = one_word->head;
+    while (tmp != NULL)
+    {
+        if (tmp->key == one_key)
+        {
+            return (tmp->value);
+        }
+        tmp = tmp->next;
+    }
+    return 0;
+}
+
+void word_remove_first(word_t *one_word)
+{
+    assert(one_word != NULL);
+
+    element_t *tmp = one_word->head;
+    one_word->head = one_word->head->next;
+    free(tmp);
+}
+
+char word_get_key(element_t *one_element)
+{
+    assert(one_element != NULL);
+    return one_element->key;
+}
+
+int word_get_value(element_t *one_element)
+{
+    assert(one_element != NULL);
+    return one_element->value;
+}
+
+int word_get_size(word_t *one_word)
+{
+    assert(one_word != NULL);
+    int size = 0;
+    element_t *tmp = one_word->head;
+    while (tmp != NULL)
+    {
+        size++;
+        tmp = tmp->next;
+    }
+    return size;
+}
+
+word_t *word_copy(word_t *one_word)
+{
+    assert(one_word != NULL);
+    word_t *new_word = word_create();
+    element_t *tmp = one_word->head;
+    while (tmp != NULL)
+    {
+        word_append(new_word, tmp->key, tmp->value);
+        tmp = tmp->next;
+    }
+    return new_word;
+}
+
 list_t *list_copy(list_t *one_list)
 {
     assert(one_list != NULL);
     list_t *new_list = list_create();
-    element_t *tmp = one_list->head;
+    word_t *tmp = one_list->sublist;
     while (tmp != NULL)
     {
-        list_append(new_list, tmp->key, tmp->value);
+        list_append(new_list, tmp);
         tmp = tmp->next;
     }
     return new_list;
+}
+
+char* word_to_char(word_t *one_word){
+    assert(one_word != NULL);
+
+    bool first = true;
+    char* mot = "";
+    element_t *current = one_word->head;
+    while (current != NULL)
+    {
+        if (!first)
+        mot = mot + current->key;
+        first = false;
+        current = current->next;
+    }
+
+    return mot;
+}
+
+word_t* char_to_word(char *one_word){
+    assert(one_word != NULL);
+    word_t* w = word_create();
+    for (size_t i = 0; i < strlen(one_word); i++)
+    {
+        word_append(w,one_word[i],0);
+    }
+    return w;
 }
--- solver/src/list.h
+++ solver/src/list.h
@@ -12,12 +12,22 @@ struct _element_t
 
 typedef struct _element_t element_t;
 
-struct _list_t
+struct _word_t
 {
     element_t *head;
+    struct _word_t *next;
+};
+
+typedef struct _word_t word_t;
+
+struct _list_t
+{
+    word_t* sublist;
 };
 
 typedef struct _list_t list_t;
+
+
 
 list_t *list_create();
 
@@ -25,24 +35,44 @@ void list_destroy(list_t *one_list);
 
 bool list_is_empty(list_t *one_list);
 
-void list_append(list_t *one_list, char one_key, int one_value);
-
-void element_print(element_t *one_element);
+void list_append(list_t *one_list, word_t* one_word);
 
 void list_print(list_t *one_list);
 
-bool list_contains(list_t *one_list, char one_key);
-
-int list_find(list_t *one_list, char one_key);
-
-void list_remove_first(list_t *one_list);
-
-char list_get_key(element_t *one_element);
-
-int list_get_value(element_t *one_element);
+bool list_contains(list_t *one_list, word_t* one_word);
 
 int list_get_size(list_t *one_list);
 
 list_t *list_copy(list_t *one_list);
+
+word_t *word_create();
+
+void word_destroy(word_t *one_word);
+
+bool word_is_empty(word_t *one_word);
+
+void word_append(word_t *one_word, char one_key, int one_value);
+
+void element_print(element_t *one_element);
+
+void word_print(word_t *one_word);
+
+bool word_contains(word_t *one_word, char one_key);
+
+int word_find(word_t *one_word, char one_key);
+
+void word_remove_first(word_t *one_word);
+
+char word_get_key(element_t *one_element);
+
+int word_get_value(element_t *one_element);
+
+int word_get_size(word_t *one_word);
+
+word_t *word_copy(word_t *one_word);
+
+char* word_to_char(word_t *one_word);
+
+word_t* char_to_word(char *one_word);
 
 #endif /* __LIST_H__ */
