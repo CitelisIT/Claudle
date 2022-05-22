e005c6880b1cffdafa335e9b9d8001d412255c34
Guillaume RICARD <guillaume.ricard@telecomnancy.eu>
2022-05-22 17:12:44 +0200

    [fix] Fixed the creation size of a dictionnary to match with our dictionaries

--- solver/src/dico.c
+++ solver/src/dico.c
@@ -1,27 +1,27 @@
-#include"dico.h"
+#include "dico.h"
 
-
-    table_t* dico_load(char* filename){
-        table_t* table = table_create(10000);
-        FILE* ptr = fopen(filename, "r");
-        char ch[30] = "";
-        if (ptr == NULL) {
-            printf("file can't be opened \n");
-            return NULL;
-        }
-        
-        while(fgets(ch, 30, ptr) != NULL){
-            for (int i = 0; i < strlen(ch); i++)
-            {
-                if (ch[i] == '\n' || ch[i] == '\r')
-                {
-                    ch[i] = '\0';
-                }
-            }            
-            bool added = table_add(table, ch);
-            
-        }
-        fclose(ptr);
-        return table;
+table_t *dico_load(char *filename)
+{
+    table_t *table = table_create(133764);
+    FILE *ptr = fopen(filename, "r");
+    char ch[30] = "";
+    if (ptr == NULL)
+    {
+        printf("file can't be opened \n");
+        return NULL;
     }
-    
+
+    while (fgets(ch, 30, ptr) != NULL)
+    {
+        for (int i = 0; i < strlen(ch); i++)
+        {
+            if (ch[i] == '\n' || ch[i] == '\r')
+            {
+                ch[i] = '\0';
+            }
+        }
+        bool added = table_add(table, ch);
+    }
+    fclose(ptr);
+    return table;
+}
