f55f5972b6133b15f84214f2362b5d750fe5cf37
BENE Tom <tom.bene@telecomnancy.eu>
2022-05-26 14:13:11 +0200

    [test] updated tests to reflect implementation of user_input type

--- solver/tests/io_tests.c
+++ solver/tests/io_tests.c
@@ -4,24 +4,22 @@
 
 int main()
 {
-    int *lines = read_word_lengths();
-    for (int i = 0; i < 5; i++)
-    {
-        printf("%d\n", lines[i]);
-    }
+    int l = read_word_lengths();
+    printf("%d\n", l);
     error_t *error = (error_t *)calloc(1, sizeof(error_t));
     printf("input \"01\" is valid : %d\n", validate_input("01", 5, error));
     printf("input \"011111\" is valid : %d\n", validate_input("011111", 5, error));
     printf("input \"01213\" is valid : %d\n", validate_input("01213", 5, error));
     printf("input \"01210\" is valid : %d\n", validate_input("01210", 5, error));
-    int *hints = get_hints(5);
+    user_input *hints = get_hints(5);
     printf("hints: ");
     for (int i = 0; i < 5; i++)
     {
-        printf("%d", hints[i]);
+        printf("%d", hints->response[i]);
     }
     printf("\n");
-    free(hints);
+    printf("exited: %d\n", hints->exited);
+    printf("valid: %d\n", hints->valid);
+    destroy_user_input(hints);
     free(error);
-    free(lines);
 }
